652e41157274a4d6a470d93f8bfdddc549a651d7
leonardo_zanardi <leonardo_zanardi@sicredi.com.br>
2023-03-07 10:49:40 -0300

    initial commit

---
+++ src/main/java/com/pocreactiveapi/PocReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.pocreactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocReactiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pocreactiveapi/PocReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocreactiveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
